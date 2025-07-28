e4a8ec1d053197da36f4943dc40df77713d003b2
3D-Up <53364055+Stefan-Biller@users.noreply.github.com>
2025-07-28 17:45:27 +0200

    project scaffold: deno vite vue tailwind

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts"></script>
+
+<template>
+  <h1 class="text-3xl font-bold underline">Header // TODO</h1>
+  <p class="text-lg">Intro // TODO</p>
+
+  <ItemList id="claims">
+    <div
+      v-for="item in ['Claim 1', 'Claim 2', 'Claim 3']"
+      :key="item"
+      class="p-4 bg-gray-100 rounded shadow"
+    >
+      {{ item }}
+    </div>
+  </ItemList>
+
+  <ItemList id="counters">
+    <div
+      v-for="item in ['Counter 1', 'Counter 2', 'Counter 3']"
+      :key="item"
+      class="p-4 bg-gray-100 rounded shadow"
+    >
+      {{ item }}
+    </div>
+  </ItemList>
+  <p class="text-lg">CallToAction // TODO</p>
+  <p class="text-lg">Contact Information // TODO</p>
+</template>
---
+++ src/components/ItemList.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts"></script>
+
+<template>
+  <!-- list of items-->
+  <slot></slot>
+</template>
